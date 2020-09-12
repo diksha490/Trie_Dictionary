fc6cb5bb6013866bcd7d6e0753fbed4772b47824
diksha490 <diksha.mcs19.du@gmail.com>
2020-09-12 15:58:38 +0530

    Update Insert.java

--- src/trie/Insert.java
+++ src/trie/Insert.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trie;
 import java.util.*;
 
@@ -16,10 +11,6 @@ import java.io.*;
 import javax.swing.*;
 
 
-/**
- *
- * @author Dell
- */
 public class Insert extends javax.swing.JFrame {
 
     /**
